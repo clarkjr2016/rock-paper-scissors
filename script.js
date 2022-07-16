2b00cba6d5fbb3ed9517219ce59f3f8aded16958
Eugene Clark <clarkjr2016@gmail.com>
2022-07-15 21:49:21 -0400

    Looseley completed the project!!! But I have some things that i can button up

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/* Add some variables here so that you have some "counting" variables in a global scope 
+to refrence in functions.
+*/
+
+let playerCount = 0;
+let compCount = 0;
+
 function computerPlay() {
     let play = Math.round(Math.random() * 100);
     if (play <= 33) {
@@ -9,62 +16,69 @@ function computerPlay() {
     else {
         return "Paper";
     }
-}
+} // Randomly generates a return value of either "Rock", "Paper", "Scissors".
 
 function playRound(playerSelection, computerSelection = computerPlay()) {
     const player = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1); // This makes the user input entry case insensitive
     const comp = computerSelection;
-    let playerResults;
+    
 
 
      if (player === "Rock") {
          if (comp === "Rock") {
-            playerResults = null;
-            compResults = null;
-             return `It's a tie! You both chose ${comp}`;
-             
+             playerCount += 0;
+             compCount += 0;
+             return `It's a tie! You both chose ${comp}`;    
          }
         else if (comp === "Scissors") {
-            
+            playerCount += 1;
+            compCount += 0;
             return `You win! ${player} beats ${comp}`;
-            
         }
         else {
-            
+            playerCount += 0;
+            compCount += 1;
             return `You lose! ${comp} beats ${player}`;
             
         }
+
      };
      if (player === "Scissors") {
         if (comp === "Scissors") {
-            
+            playerCount += 0;
+            compCount += 0;
             return `It's a tie! You both chose ${comp}`
             
         }
        else if (comp === "Paper") {
-        
-           return `You win! ${player} beats ${comp}`
+            playerCount += 1;
+            compCount += 0;
+         return `You win! ${player} beats ${comp}`
            
        }
        else {
-        
-           return `You lose! ${comp} beats ${player}`
-           
+        playerCount += 0;
+        compCount += 1;
+           return `You lose! ${comp} beats ${player}`   
        }
     };
 
     if (player === "Paper") {
         if (comp === "Paper") {
-            
+            playerCount += 0;
+            compCount += 0;
             return `It's a tie! You both chose ${comp}`
             
         }
        else if (comp === "Rock") {
-        
+            playerCount += 1;
+            compCount += 0;
            return `You win! ${player} beats ${comp}`
            
        }
        else {
+        playerCount += 0;
+        compCount += 1;
            return `You lose! ${comp} beats ${player}`
            
        }
@@ -74,11 +88,16 @@ function playRound(playerSelection, computerSelection = computerPlay()) {
 function game() {
     for (let i = 0; i < 5; i++) {
 
-        let compCount = 0
-        let playerCount = 0
-playRound(prompt("Rock Paper or Scissors"));
-        
+        playRound(prompt("Rock Paper or Scissors?"));
+        console.log(`Player score: ${playerCount}`);
+        console.log(`Computer score: ${compCount}`);
         }
+    if (playerCount > compCount) {
+        console.log("YOU WON BABY!!");
+    }
+    else {
+        console.log("SORRY YOU LOSS :(")
+    }
     
 
 }
